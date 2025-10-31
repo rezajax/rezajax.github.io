503c3a2abe170b3163474d65450332d4cbc81bcb
Reza <rezajaxg@gmail.com>
2025-10-31 04:33:17 +0400

    push

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧑‍💻 Reza Blog",
+    pageTitle: "Reza",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
